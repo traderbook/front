d563efa1438c7d242448f346e8d23e42e4e540aa
Aurélien <aurelien.martin@niji.fr>
2018-01-29 15:59:41 +0100

    Add content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header.js'
+import Content from './components/Content/Content.js'
 import Footer from './components/Footer/Footer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to Traderbook</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        <Footer/>
+        <Header />
+        <Content />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Content/Content.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 30,
+  },
+  paper: {
+    padding: 16,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+function Content(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}><h2>Bloc code</h2></Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}><h2>Bloc Params</h2></Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+Content.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Content);
