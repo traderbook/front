dea8b0e1a6f42427a48dc2f9e744343823c5b151
Gsln44 <dylan.gosselin44@hotmail.fr>
2018-03-26 16:38:24 +0200

    partie doc

--- src/components/Content/Options/Content/Documentation/Documentation.react.js
+++ src/components/Content/Options/Content/Documentation/Documentation.react.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Documentation.scss';
 
 
 export default class OptionsContentDocumentation extends React.Component {
@@ -9,10 +10,21 @@ export default class OptionsContentDocumentation extends React.Component {
 
   render() {
     return (
-      <div>
-        <p>Documentation</p>
+      <div id="content">
+        <div id="left">
+          <h2>Event function</h2>
+          <li>OnInit</li>
+          <li>OnTick</li>
+          <li>OnDestruct</li>
+          <h2>Trading function</h2>
+          <li>OpenOrder</li>
+          <li>CloseOrder</li>
+          <li>ListOrder</li>
+        </div>
+        <div id="right">
+          <h2>test</h2>
+        </div>
       </div>
-
     )
   }
 }
