ca2ab0352e3c0905c2cbdd8ded7f9d5434c02cee
scostanzo <sischioandroid@gmail.com>
2018-03-26 15:13:29 +0200

    lorem ipsum in output tab

--- src/components/Content/Options/Content/Output/Output.react.js
+++ src/components/Content/Options/Content/Output/Output.react.js
@@ -2,17 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
+function createData(date, message) {
+  return { date, message };
+}
+
+const data = [
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  createData('01-01-1970 00:00:00', 'Lorem ipsum dolor sit amet'),
+  ];
+
 export default class OptionsContentOutput extends React.Component {
   constructor(props) {
     super(props)
   }
 
+
+
   render() {
     return (
       <div>
-        <p>Output</p>
+        {data.map(n => {
+              return (
+                  <p>[{n.date}] - {n.message} </p>
+              );
+            })}
       </div>
-
     )
   }
 }
