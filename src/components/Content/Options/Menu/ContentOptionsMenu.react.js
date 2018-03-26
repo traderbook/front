9d5872802bb41008d3cfa1da45fd5b385cfd0919
Aurelien <aurelien.radio@gmail.com>
2018-03-26 13:09:52 +0200

    Add tab in backtests route

--- src/components/Content/Options/Menu/ContentOptionsMenu.react.js
+++ src/components/Content/Options/Menu/ContentOptionsMenu.react.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OptionsContentAnalytics from '../Content/Analytics/Analytics.react';
+import OptionsContentBacktests from '../Content/Backtests/Backtests.react';
 import OptionsContentOutput from '../Content/Output/Output.react';
 import OptionsContentDocumentation from '../Content/Documentation/Documentation.react';
 
@@ -22,9 +23,12 @@ export default class ContentOptionsMenu extends React.Component {
                     <li><Link to="/output">Output</Link></li>
                     <li><Link to="/documentation">Documentation</Link></li>
                 </ul>
+                <div>
                 <Route path="/analytics" component={OptionsContentAnalytics} />
+                <Route path="/backtests" component={OptionsContentBacktests} />
                 <Route path="/output" component={OptionsContentOutput} />
                 <Route path="/documentation" component={OptionsContentDocumentation} />
+                </div>
             </div>
         </Router>
     )
@@ -34,5 +38,6 @@ export default class ContentOptionsMenu extends React.Component {
   const styles = {
     menu: {
       display: 'flex',
+      flexDirection: 'column'
     }
   }
