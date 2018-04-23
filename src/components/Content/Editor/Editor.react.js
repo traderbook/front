521571456b38d841b1a4933f6934dff3ce326813
Dorian Latouche <Dorian.latouche@hotmail.com>
2018-04-23 09:52:28 +0200

    Ajout snackbar lors sauvegarde

--- src/components/Content/Editor/Editor.react.js
+++ src/components/Content/Editor/Editor.react.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import AceEditor from 'react-ace'
-import './Editor.scss'
-import 'brace/mode/javascript'
-import 'brace/theme/monokai'
-import 'brace/snippets/javascript'
-import 'brace/ext/language_tools'
-import Toolbar from 'material-ui/Toolbar'
-import IconButton from 'material-ui/IconButton'
-import Icon from 'material-ui/Icon'
-import Input from 'material-ui/Input'
-import Menu, { MenuItem } from 'material-ui/Menu'
-import DialogAddConfig from './DialogAddConfig.react'
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import AceEditor from "react-ace"
+import "./Editor.scss"
+import "brace/mode/javascript"
+import "brace/theme/monokai"
+import "brace/snippets/javascript"
+import "brace/ext/language_tools"
+import Toolbar from "material-ui/Toolbar"
+import IconButton from "material-ui/IconButton"
+import Icon from "material-ui/Icon"
+import Input from "material-ui/Input"
+import Menu, { MenuItem } from "material-ui/Menu"
+import DialogAddConfig from "./DialogAddConfig.react"
+import Snackbar from "material-ui/Snackbar"
 
 export default class Editor extends Component {
 	constructor(props, context) {
 		super(props, context)
 		this.state = {
-			code: 0, anchorEl: null, openAlgo: false, openMenuConfig: false, anchorElConfig: null, nameConfig: 0, openDialogConfig: false,
-			nameAlgo: 0, tabConfig: ['Config n°1', 'Config n°2'], tabAlgo: ['Algo n°1', 'Algo n°2'], tabCode: ['class Main extends Strategy {\n\tOnInit(){\n\n\t}\n\n\ttOnTick(tick){\n\n\t}\n\n\tOnDestruct(){\n\n\t}\n}', 'let animal = new Lapin() \nlet castor = new Castor()'],
-			nameAlgoInput: 'Algo n°1', codeValue: 'class Main extends Strategy {\n\tOnInit(){\n\n\t}\n\n\ttOnTick(tick){\n\n\t}\n\n\tOnDestruct(){\n\n\t}\n}'
+			code: 0,
+			anchorEl: null,
+			openAlgo: false,
+			openMenuConfig: false,
+			anchorElConfig: null,
+			nameConfig: 0,
+			openDialogConfig: false,
+			nameAlgo: 0,
+			tabConfig: ["Config n°1", "Config n°2"],
+			tabAlgo: ["Algo n°1", "Algo n°2"],
+			tabCode: [
+				"class Main extends Strategy {\n\tOnInit(){\n\n\t}\n\n\tOnTick(tick){\n\n\t}\n\n\tOnDestruct(){\n\n\t}\n}",
+				"let animal = new Lapin() \nlet castor = new Castor()"
+			],
+			nameAlgoInput: "Algo n°1",
+			codeValue:
+				"class Main extends Strategy {\n\tOnInit(){\n\n\t}\n\n\tOnTick(tick){\n\n\t}\n\n\tOnDestruct(){\n\n\t}\n}",
+			openSnack: false
 		}
 	}
 
@@ -27,131 +43,230 @@ export default class Editor extends Component {
 		this.setState({ codeValue: code })
 	}
 
-	play = () => {
-		console.log('Editor.react.js -> 25 : Play', )
+	closeSnack = () => {
+		this.setState({ openSnack: false })
 	}
 
-	closeAlog = (num) => {
+	play = () => {
+		console.log("Editor.react.js -> 25 : Play")
+	}
+
+	closeAlog = num => {
 		switch (num) {
 			case -2:
-				this.setState({ openAlgo: false, code: -1, codeValue: '', nameAlgo: -1, nameAlgoInput:''})
+				this.setState({
+					openAlgo: false,
+					code: -1,
+					codeValue: "",
+					nameAlgo: -1,
+					nameAlgoInput: ""
+				})
 
 				break
 			case -1:
 				this.setState({ openAlgo: false })
 				break
 			default:
-				this.setState({ openAlgo: false, code: num, codeValue: this.state.tabCode[num], nameAlgo: num, nameAlgoInput: this.state.tabAlgo[num] })
+				this.setState({
+					openAlgo: false,
+					code: num,
+					codeValue: this.state.tabCode[num],
+					nameAlgo: num,
+					nameAlgoInput: this.state.tabAlgo[num]
+				})
 				break
 		}
 	}
 
 	onClickAddConfiguration = () => {
-		this.setState({ openMenuConfig: false, nameConfig: 'Config n°1', openDialogConfig: true })
+		this.setState({
+			openMenuConfig: false,
+			nameConfig: "Config n°1",
+			openDialogConfig: true
+		})
 	}
 
-	openMenu = (e) => { this.setState({ anchorEl: e.currentTarget, openAlgo: true }) }
+	openMenu = e => {
+		this.setState({ anchorEl: e.currentTarget, openAlgo: true })
+	}
 
-	addConfig = (config) => {
+	addConfig = config => {
 		let tabConfig = this.state.tabConfig
 		tabConfig.push(config.nom)
-		this.setState({ tabConfig: tabConfig, nameConfig: (tabConfig.length - 1) })
+		this.setState({
+			tabConfig: tabConfig,
+			nameConfig: tabConfig.length - 1
+		})
 	}
 
-	onKeyDown = (e) => {
-		console.log('Editor.react.js -> 66 : e.wi', e.which )
-		if (e.which == 83 && e.ctrlKey){
+	onKeyDown = e => {
+		console.log("Editor.react.js -> 66 : e.wi", e.which)
+		if (e.which == 83 && e.ctrlKey) {
 			e.preventDefault()
-			console.log('Editor.react.js -> 69 : ',this.state.nameAlgo  )
-			if(this.state.nameAlgo < 0){
+			console.log("Editor.react.js -> 69 : ", this.state.nameAlgo)
+			if (this.state.nameAlgo < 0) {
 				let tabCode = this.state.tabCode
 				tabCode.push(this.state.codeValue)
 				let tabAlgo = this.state.tabAlgo
 				tabAlgo.push(this.state.nameAlgoInput)
-				console.log('Editor.react.js -> 75 : tab', tabAlgo )
-				this.setState({tabCode: tabCode, code:tabCode.length -1, tabAlgo:tabAlgo})
-			}else{
+				console.log("Editor.react.js -> 75 : tab", tabAlgo)
+				this.setState({
+					tabCode: tabCode,
+					code: tabCode.length - 1,
+					tabAlgo: tabAlgo
+				})
+			} else {
 				let tabCode = this.state.tabCode
 				tabCode[this.state.code] = this.state.codeValue
-				this.setState({tabCode: tabCode})
+				this.setState({ tabCode: tabCode })
 			}
-			console.log('Editor.react.js -> 67 : pressed',  )
+			this.setState({openSnack: true})
+			console.log("Editor.react.js -> 67 : pressed")
 		}
 	}
 
 	render() {
 		return (
 			<div onKeyDown={this.onKeyDown}>
+				<Snackbar
+					anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+					open={this.state.openSnack}
+					onClose={this.closeSnack}
+					SnackbarContentProps={{
+						"aria-describedby": "message-id"
+					}}
+					autoHideDuration={1500}
+					message={<span id="message-id">Sauvegardé !</span>}
+				/>
 				<div>
-					<Toolbar style={{ display: 'flex' }} >
+					<Toolbar style={{ display: "flex" }}>
 						<div className="containerMenuAlgo">
 							<div>
 								<Input
 									className="inputNameAlgo"
 									placeholder="Saisir le nom de l'algorithme "
 									inputProps={{
-										'aria-label': 'Description',
+										"aria-label": "Description"
 									}}
 									value={this.state.nameAlgoInput}
-									onChange={(e) => { this.setState({ nameAlgoInput: e.target.value }) }}
+									onChange={e => {
+										this.setState({
+											nameAlgoInput: e.target.value
+										})
+									}}
 								/>
-								<IconButton onClick={this.openMenu} aria-owns={this.state.anchorEl ? 'menu-editor' : null} color="inherit" aria-label="Menu">
+								<IconButton
+									onClick={this.openMenu}
+									aria-owns={
+										this.state.anchorEl
+											? "menu-editor"
+											: null
+									}
+									color="inherit"
+									aria-label="Menu"
+								>
 									<Icon>arrow_drop_down</Icon>
 								</IconButton>
 								<Menu
 									id="menu-editor"
 									anchorEl={this.state.anchorEl}
 									anchorOrigin={{
-										vertical: 'top',
-										horizontal: 'right',
-									}
-									}
+										vertical: "top",
+										horizontal: "right"
+									}}
 									transformOrigin={{
-										vertical: 'top',
-										horizontal: 'right',
-									}
-									}
+										vertical: "top",
+										horizontal: "right"
+									}}
 									open={this.state.openAlgo}
-									onClose={() => { this.closeAlog(-1) }}
+									onClose={() => {
+										this.closeAlog(-1)
+									}}
 									className="menuAlgo"
 								>
-									{
-										this.state.tabAlgo.map(
-										(algo, i) => {
-											return (<MenuItem key={i} onClick={() => {this.closeAlog(i)}}>{algo}</MenuItem>)
-										}
-									)
-									}
-									<MenuItem onClick={() => { this.closeAlog(-2) }}>Ajouter un algorithme</MenuItem>
+									{this.state.tabAlgo.map((algo, i) => {
+										return (
+											<MenuItem
+												key={i}
+												onClick={() => {
+													this.closeAlog(i)
+												}}
+											>
+												{algo}
+											</MenuItem>
+										)
+									})}
+									<MenuItem
+										onClick={() => {
+											this.closeAlog(-2)
+										}}
+									>
+										Ajouter un algorithme
+									</MenuItem>
 								</Menu>
 							</div>
 							<div className="containerMenuRight">
-								<p>{this.state.nameConfig > -1 ? this.state.tabConfig[this.state.nameConfig] : this.state.nameConfig}</p>
-								<IconButton onClick={(e) => { this.setState({ openMenuConfig: true, anchorElConfig: e.currentTarget }) }} aria-owns={this.state.anchorElConfig ? 'menu-editorConfig' : null} color="inherit" aria-label="Menu">
+								<p>
+									{this.state.nameConfig > -1
+										? this.state.tabConfig[
+												this.state.nameConfig
+										  ]
+										: this.state.nameConfig}
+								</p>
+								<IconButton
+									onClick={e => {
+										this.setState({
+											openMenuConfig: true,
+											anchorElConfig: e.currentTarget
+										})
+									}}
+									aria-owns={
+										this.state.anchorElConfig
+											? "menu-editorConfig"
+											: null
+									}
+									color="inherit"
+									aria-label="Menu"
+								>
 									<Icon>arrow_drop_down</Icon>
 								</IconButton>
 								<Menu
 									id="menu-editorConfig"
 									anchorEl={this.state.anchorElConfig}
 									anchorOrigin={{
-										vertical: 'top',
-										horizontal: 'right',
-									}
-									}
+										vertical: "top",
+										horizontal: "right"
+									}}
 									transformOrigin={{
-										vertical: 'top',
-										horizontal: 'right',
-									}
-									}
+										vertical: "top",
+										horizontal: "right"
+									}}
 									open={this.state.openMenuConfig}
-									onClose={() => { this.setState({ openMenuConfig: false }) }}
+									onClose={() => {
+										this.setState({ openMenuConfig: false })
+									}}
 									className="menuAlgo"
 								>
 									{this.state.tabConfig.map((config, i) => {
-										return (<MenuItem key={i} onClick={() => { this.setState({ openMenuConfig: false, nameConfig: i }) }}>{config}</MenuItem>)
+										return (
+											<MenuItem
+												key={i}
+												onClick={() => {
+													this.setState({
+														openMenuConfig: false,
+														nameConfig: i
+													})
+												}}
+											>
+												{config}
+											</MenuItem>
+										)
 									})}
-									<MenuItem onClick={this.onClickAddConfiguration}>Ajouter une configuration</MenuItem>
-
+									<MenuItem
+										onClick={this.onClickAddConfiguration}
+									>
+										Ajouter une configuration
+									</MenuItem>
 								</Menu>
 								<div className="iconColorPlay">
 									<IconButton
@@ -183,10 +298,16 @@ export default class Editor extends Component {
 						enableLiveAutocompletion: true,
 						enableSnippets: true,
 						showLineNumbers: true,
-						tabSize: 4,
+						tabSize: 4
 					}}
 				/>
-				<DialogAddConfig open={this.state.openDialogConfig} add={this.addConfig} close={() => { this.setState({ openDialogConfig: false }) }} />
+				<DialogAddConfig
+					open={this.state.openDialogConfig}
+					add={this.addConfig}
+					close={() => {
+						this.setState({ openDialogConfig: false })
+					}}
+				/>
 			</div>
 		)
 	}
